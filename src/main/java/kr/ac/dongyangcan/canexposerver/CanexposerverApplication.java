68da1365493f30c78b934f54a3a98d0f06c189ef
jnjosjk0965 <wnwotjr8@naver.com>
2024-07-15 18:58:59 +0900

    start project

---
+++ src/main/java/kr/ac/dongyangcan/canexposerver/CanexposerverApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.dongyangcan.canexposerver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanexposerverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanexposerverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/dongyangcan/canexposerver/CanexposerverApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.dongyangcan.canexposerver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanexposerverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
